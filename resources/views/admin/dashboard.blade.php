68cf853c68cdf554add1c4917b027da04a0518b6
vit-posmourny <dr1v3r@email.cz>
2025-04-16 04:53:02 +0200

    added admin.dashboard.blade

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,4 @@
-<!-- resources\views\dashboard.blade.php -->
+<!-- resources\views\admin\dashboard.blade.php -->
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+// must be middleware(['auth:admin',...]) here, for an admin
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth:admin', 'verified'])->name('admin.dashboard');
 
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
