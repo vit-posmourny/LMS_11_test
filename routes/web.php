c5d8fb5fd5d49bdd498bb393eac6ddc2166993fe
vit-posmourny <dr1v3r@email.cz>
2025-04-22 01:31:19 +0200

    deleted '/profile' routes from web.php

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 // must be middleware(['auth:admin',...]) here, for an admin
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard');
