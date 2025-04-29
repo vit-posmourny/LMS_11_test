d890e5da0713f67894b786ace7abba90acb8c366
vit-posmourny <dr1v3r@email.cz>
2025-04-29 03:30:09 +0200

    add admin/DashboardController & admin/layouts/master.blade

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    function index(): View
+    {
+        return view('admin.layouts.master');
+    }
 }
