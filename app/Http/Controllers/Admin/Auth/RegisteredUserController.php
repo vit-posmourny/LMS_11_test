106563f2fb50e778bf932b3491e419c80a362b2e
vit-posmourny <dr1v3r@email.cz>
2025-05-10 20:36:49 +0200

    8. 8_Admin Panel - Remove Registration Form

--- app/Http/Controllers/Admin/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Admin/Auth/RegisteredUserController.php
@@ -15,14 +15,6 @@ use Illuminate\View\View;
 class RegisteredUserController extends Controller
 {
     /**
-     * Display the registration view.
-     */
-    public function create(): View
-    {
-        return view('auth.register');
-    }
-
-    /**
      * Handle an incoming registration request.
      *
      * @throws \Illuminate\Validation\ValidationException
--- routes/admin.php
+++ routes/admin.php
@@ -13,8 +13,6 @@ use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(["middleware" => "guest", "prefix" => "admin", "as" => "admin."], function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
