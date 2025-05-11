5ad1d3094c9173b0ca80f5b82315a0b52ebfe817
vit-posmourny <dr1v3r@email.cz>
2025-05-11 02:21:53 +0200

    2. 2_Frontend Panel - Mastering Home Page

---
+++ resources/views/frontend/pages/home/index.blade.php
@@ -0,0 +1,119 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+   <!--===========================
+        HEADER START
+    ============================-->
+    @include('frontend.layouts.header')
+    <!--===========================
+        HEADER END
+    ============================-->
+
+
+    <!--===========================
+        BANNER 3 START
+    ============================-->
+    @include('frontend.pages.sections.hero-section')
+    <!--===========================
+        BANNER 3 END
+    ============================-->
+
+
+    <!--===========================
+        CATEGORY 4 START
+    ============================-->
+    @include('frontend.pages.sections.category-section')
+    <!--===========================
+        CATEGORY 4 END
+    ============================-->
+
+
+    <!--===========================
+        ABOUT 3 START
+    ============================-->
+    @include('frontend.pages.sections.about-section')
+    <!--===========================
+        ABOUT 3 END
+    ============================-->
+
+
+    <!--===========================
+        COURSES 3 START
+    ============================-->
+    @include('frontend.pages.sections.courses-section')
+    <!--===========================
+        COURSES 3 END
+    ============================-->
+
+
+    <!--===========================
+        OFFER START
+    ============================-->
+    @include('frontend.pages.sections.offer-section')
+    <!--===========================
+        OFFER END
+    ============================-->
+
+
+    <!--===========================
+        BECOME INSTRUCTOR START
+    ============================-->
+    @include('frontend.pages.sections.become-instructor-section')
+    <!--===========================
+        BECOME INSTRUCTOR END
+    ============================-->
+
+
+    <!--===========================
+        VIDEO START
+    ============================-->
+    @include('frontend.pages.sections.video-section')
+    <!--===========================
+        VIDEO END
+    ============================-->
+
+
+    <!--===========================
+        BRAND START
+    ============================-->
+    @include('frontend.pages.sections.brand-section')
+    <!--===========================
+        BRAND END
+    ============================-->
+
+
+    <!--===========================
+        QUALITY COURSES START
+    ============================-->
+    @include('frontend.pages.sections.quality-courses-section')
+    <!--===========================
+        QUALITY COURSES END
+    ============================-->
+
+
+    <!--===========================
+        TESTIMONIAL START
+    ============================-->
+    @include('frontend.pages.sections.testimonial-start-section')
+    <!--===========================
+        TESTIMONIAL END
+    ============================-->
+
+
+    <!--===========================
+        BLOG 4 START
+    ============================-->
+    @include('frontend.pages.sections.blog-section')
+    <!--===========================
+        BLOG 4 END
+    ============================-->
+
+
+    <!--===========================
+        FOOTER 3 START
+    ============================-->
+    @include('frontend.layouts.footer')
+    <!--===========================
+        FOOTER 3 END
+    ============================-->
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,31 @@
 <?php
 
+use App\Http\Controllers\Frontend\FrontendController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\UserDashboardController;
 use App\Http\Controllers\Frontend\InstructorDashboardContoller;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+/*
+*-----------------------------------------------------------------
+*   Frontend Routes
+*-----------------------------------------------------------------
+*/
+Route::get('/', [FrontendController::class, 'index'])->name('home');
 /*
 *-----------------------------------------------------------------
 *   Student Routes
 *-----------------------------------------------------------------
 */
-
 Route::group(['middleware' => ['auth:web', 'verified', 'check_role:student'], 'prefix' => 'student', 'as' => 'student.'], function() {
     Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
 });
 
-
 /*
 *-----------------------------------------------------------------
 *   Student Routes
 *-----------------------------------------------------------------
 */
-
 Route::group(['middleware' => ['auth:web', 'verified', 'check_role:instructor'], 'prefix' => 'instructor', 'as' => 'instructor.'], function() {
     Route::get('/dashboard', [InstructorDashboardContoller::class, 'index'])->name('dashboard');
 });
