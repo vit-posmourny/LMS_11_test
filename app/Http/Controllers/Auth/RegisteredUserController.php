e7c42ef41de124b198a724c4ea1c8ab371a7bda0
vit-posmourny <dr1v3r@email.cz>
2025-05-21 22:08:05 +0200

    4. 4_Frontend Panel - Mastering Register Page

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -27,7 +26,7 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request)
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
@@ -44,7 +43,5 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
-
-        return redirect(route('dashboard', absolute: false));
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,123 +1,72 @@
-{{-- <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+<!--===========================
+    SIGN IN START
+============================-->
+<section class="wsus__sign_in" style="margin-top: 80px">
+    <div class="row align-items-center">
+        <div class="col-xxl-5 col-xl-6 col-lg-6 wow fadeInLeft">
+            <div class="wsus__sign_img">
+                <img src="{{ asset('frontend/assets/images/login_img_1.jpg') }}" alt="login" class="img-fluid">
+                <a href="index.html">
+                    <img src="{{ asset('frontend/assets/images/logo.png') }}" alt="EduCore" class="img-fluid">
                 </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout> --}}
-
-@extends('frontend.layouts.master')
-
-@section('content')
-    <!--===========================
-        SIGN IN START
-    ============================-->
-    <section class="wsus__sign_in" style="margin-top: 80px">
-        <div class="row align-items-center">
-            <div class="col-xxl-5 col-xl-6 col-lg-6 wow fadeInLeft">
-                <div class="wsus__sign_img">
-                    <img src="{{ asset('frontend/assets/images/login_img_1.jpg') }}" alt="login" class="img-fluid">
-                    <a href="index.html">
-                        <img src="{{ asset('frontend/assets/images/logo.png') }}" alt="EduCore" class="img-fluid">
-                    </a>
-                </div>
             </div>
-            <div class="col-xxl-4 col-xl-5 col-lg-6 col-md-9 m-auto wow fadeInRight">
-                <div class="wsus__sign_form_area">
-                    <div class="tab-content" id="pills-tabContent">
-                        <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
-                            aria-labelledby="pills-home-tab" tabindex="0">
-                            <form action="{{ route('login')}}" method="POST">
-                                @csrf
-                                <h2>Log in<span>!</span></h2>
-                                <div class="row">
-                                    <div class="col-xl-12">
-                                        <div class="wsus__login_form_input">
-                                            <label>Email</label>
-                                            <input type="email" name="email" value="{{ old('email')}}" placeholder="Email" required>
-                                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                                        </div>
-                                    </div>
-                                    <div class="col-xl-12">
-                                        <div class="wsus__login_form_input">
-                                            <label>Password<a href="{{ route('password.request') }}">Forgot Password?</a></label>
-                                            <input type="password" name="password" placeholder="Password" required>
-                                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                                        </div>
-                                    </div>
-                                    <div class="col-xl-12">
-                                        <div class="wsus__login_form_input">
-                                            <div class="form-check">
-                                                <input class="form-check-input" type="checkbox" name="remember" value=""
-                                                    id="flexCheckDefault">
-                                                <label class="form-check-label" for="flexCheckDefault">
-                                                    Remember Me
-                                                </label>
-                                            </div>
-                                            <button type="submit" class="common_btn">Sign In</button>
-                                        </div>
+        </div>
+        <div class="col-xxl-4 col-xl-5 col-lg-6 col-md-9 m-auto wow fadeInRight">
+            <div class="wsus__sign_form_area">
+                <div class="tab-content" id="pills-tabContent">
+                    <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
+                        aria-labelledby="pills-home-tab" tabindex="0">
+                        <form action="{{ route('login')}}" method="POST">
+                            @csrf
+                            <h2>Log in<span>!</span></h2>
+                            <div class="row">
+                                <div class="col-xl-12">
+                                    <div class="wsus__login_form_input">
+                                        <label>Email</label>
+                                        <input type="email" name="email" value="{{ old('email')}}" placeholder="Email" required>
+                                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
                                     </div>
                                 </div>
-                            </form>
-                            {{-- <p class="or">or</p>
-                            <ul class="social_login d-flex flex-wrap">
-                                <li>
-                                    <a href="#">
-                                        <span><img src="{{ asset('frontend/assets/images/google_icon.png') }}" alt="Google" class="img-fluid"></span>
-                                        Google
-                                    </a>
-                                </li>
-                            </ul> --}}
-                            <p class="create_account">Don't have an account? <a href="{{ route('register')}}">Create free
-                                    account</a></p>
-                        </div>
+                                <div class="col-xl-12">
+                                    <div class="wsus__login_form_input">
+                                        <label>Password<a href="{{ route('password.request') }}">Forgot Password?</a></label>
+                                        <input type="password" name="password" placeholder="Password" required>
+                                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                    </div>
+                                </div>
+                                <div class="col-xl-12">
+                                    <div class="wsus__login_form_input">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="remember" value=""
+                                                id="flexCheckDefault">
+                                            <label class="form-check-label" for="flexCheckDefault">
+                                                Remember Me
+                                            </label>
+                                        </div>
+                                        <button type="submit" class="common_btn">Sign In</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        {{-- <p class="or">or</p>
+                        <ul class="social_login d-flex flex-wrap">
+                            <li>
+                                <a href="#">
+                                    <span><img src="{{ asset('frontend/assets/images/google_icon.png') }}" alt="Google" class="img-fluid"></span>
+                                    Google
+                                </a>
+                            </li>
+                        </ul> --}}
+                        <p class="create_account">Don't have an account? <a href="{{ route('register')}}">Create free
+                                account</a></p>
                     </div>
                 </div>
             </div>
         </div>
-        <a class="back_btn" href="index.html">Back to Home</a>
-    </section>
-    <!--===========================
-        SIGN IN END
-    ============================-->
-@endsection
+    </div>
+    <a class="back_btn" href="index.html">Back to Home</a>
+</section>
+<!--===========================
+    SIGN IN END
+============================-->
