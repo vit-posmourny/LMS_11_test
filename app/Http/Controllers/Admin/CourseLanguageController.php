ba24794a47658bec352e9f7e33e5dcd38c2e8b9e
vit-posmourny <dr1v3r@email.cz>
2025-06-16 23:24:06 +0200

    35 - 3. 3_Course Language - Working with Create Feature (Part -2)

--- app/Http/Controllers/Admin/CourseLanguageController.php
+++ app/Http/Controllers/Admin/CourseLanguageController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\CourseLanguage;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class CourseLanguageController extends Controller
 {
@@ -19,17 +22,28 @@ class CourseLanguageController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('admin.course.language.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $request->validate([
+            'name' => 'required|max:255|unique:course_languages',
+        ]);
+
+        $language = new CourseLanguage();
+        $language->name = $request->name;
+        $language->slug = Str::slug($request->name);
+        $language->save();
+
+        notyf()->success('Language created');
+
+        return to_route('admin.course-languages.index');
     }
 
     /**
--- resources/views/admin/course/language/index.blade.php
+++ resources/views/admin/course/language/index.blade.php
@@ -1,5 +1,4 @@
 {{-- resources\views\admin\course\language\index.blade.php --}}
-{{-- resources\views\admin\instructor-request\index.blade.php --}}
 @extends('admin.layouts.master')
 
 @section('content')
@@ -9,7 +8,7 @@
             <div class="card-header">
                 <h3 class="card-title">Course Languages</h3>
                 <div class="card-actions">
-                      <a href="#" class="btn btn-primary">
+                      <a href="{{ route('admin.course-languages.create') }}" class="btn btn-primary">
                         <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 5l0 14"></path><path d="M5 12l14 0"></path></svg>
                         Add new
