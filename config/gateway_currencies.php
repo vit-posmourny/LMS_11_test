9018a07e7eb1810204fa6e5d2256e2f2fa7f4198
vit-posmourny <dr1v3r@email.cz>
2025-08-18 18:05:50 +0200

    42 - 11. 11_Payment Setting - Working with PayPal Setting (Part-2)

---
+++ config/gateway_currencies.php
@@ -0,0 +1,174 @@
+<?php
+
+return [
+
+    'all_currencies' => [
+        'AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN',
+        'BAM', 'BBD', 'BDT', 'BGN', 'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BRL',
+        'BSD', 'BTN', 'BWP', 'BYN', 'BZD', 'CAD', 'CDF', 'CHF', 'CLP', 'CNY',
+        'COP', 'CRC', 'CUC', 'CUP', 'CVE', 'CZK', 'DJF', 'DKK', 'DOP', 'DZD',
+        'EGP', 'ERN', 'ETB', 'EUR', 'FJD', 'FKP', 'GBP', 'GEL', 'GGP', 'GHS',
+        'GIP', 'GMD', 'GNF', 'GTQ', 'GYD', 'HKD', 'HNL', 'HRK', 'HTG', 'HUF',
+        'IDR', 'ILS', 'IMP', 'INR', 'IQD', 'IRR', 'ISK', 'JEP', 'JMD', 'JOD',
+        'JPY', 'KES', 'KGS', 'KHR', 'KMF', 'KPW', 'KRW', 'KWD', 'KYD', 'KZT',
+        'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'LYD', 'MAD', 'MDL', 'MGA', 'MKD',
+        'MMK', 'MNT', 'MOP', 'MRU', 'MUR', 'MVR', 'MWK', 'MXN', 'MYR', 'MZN',
+        'NAD', 'NGN', 'NIO', 'NOK', 'NPR', 'NZD', 'OMR', 'PAB', 'PEN', 'PGK',
+        'PHP', 'PKR', 'PLN', 'PYG', 'QAR', 'RON', 'RSD', 'RUB', 'RWF', 'SAR',
+        'SBD', 'SCR', 'SDG', 'SEK', 'SGD', 'SHP', 'SLL', 'SOS', 'SPL', 'SRD',
+        'STN', 'SVC', 'SYP', 'SZL', 'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TRY',
+        'TTD', 'TVD', 'TWD', 'TZS', 'UAH', 'UGX', 'USD', 'UYU', 'UZS', 'VES',
+        'VND', 'VUV', 'WST', 'XAF', 'XCD', 'XDR', 'XOF', 'XPF', 'YER', 'ZAR',
+        'ZMW', 'ZWL'
+    ],
+
+    'paypal_currencies' => [
+        'AUD' => [
+            'name' => 'Australian Dollar',
+            'symbol' => 'A$',
+            'code' => 'AUD'
+        ],
+        'BRL' => [
+            'name' => 'Brazilian Real',
+            'symbol' => 'R$',
+            'code' => 'BRL'
+        ],
+        'CAD' => [
+            'name' => 'Canadian Dollar',
+            'symbol' => 'C$',
+            'code' => 'CAD'
+        ],
+        'CNY' => [
+            'name' => 'Chinese Renminbi',
+            'symbol' => '¥',
+            'code' => 'CNY'
+        ],
+        'CZK' => [
+            'name' => 'Czech Koruna',
+            'symbol' => 'Kč',
+            'code' => 'CZK'
+        ],
+        'DKK' => [
+            'name' => 'Danish Krone',
+            'symbol' => 'kr',
+            'code' => 'DKK'
+        ],
+        'EUR' => [
+            'name' => 'Euro',
+            'symbol' => '€',
+            'code' => 'EUR'
+        ],
+        'HKD' => [
+            'name' => 'Hong Kong Dollar',
+            'symbol' => 'HK$',
+            'code' => 'HKD'
+        ],
+        'HUF' => [
+            'name' => 'Hungarian Forint',
+            'symbol' => 'Ft',
+            'code' => 'HUF'
+        ],
+        'ILS' => [
+            'name' => 'Israeli New Shekel',
+            'symbol' => '₪',
+            'code' => 'ILS'
+        ],
+        'JPY' => [
+            'name' => 'Japanese Yen',
+            'symbol' => '¥',
+            'code' => 'JPY'
+        ],
+        'MYR' => [
+            'name' => 'Malaysian Ringgit',
+            'symbol' => 'RM',
+            'code' => 'MYR'
+        ],
+        'MXN' => [
+            'name' => 'Mexican Peso',
+            'symbol' => 'Mex$',
+            'code' => 'MXN'
+        ],
+        'TWD' => [
+            'name' => 'New Taiwan Dollar',
+            'symbol' => 'NT$',
+            'code' => 'TWD'
+        ],
+        'NZD' => [
+            'name' => 'New Zealand Dollar',
+            'symbol' => 'NZ$',
+            'code' => 'NZD'
+        ],
+        'NOK' => [
+            'name' => 'Norwegian Krone',
+            'symbol' => 'kr',
+            'code' => 'NOK'
+        ],
+        'PHP' => [
+            'name' => 'Philippine Peso',
+            'symbol' => '₱',
+            'code' => 'PHP'
+        ],
+        'PLN' => [
+            'name' => 'Polish Złoty',
+            'symbol' => 'zł',
+            'code' => 'PLN'
+        ],
+        'GBP' => [
+            'name' => 'Pound Sterling',
+            'symbol' => '£',
+            'code' => 'GBP'
+        ],
+        'RUB' => [
+            'name' => 'Russian Ruble',
+            'symbol' => '₽',
+            'code' => 'RUB'
+        ],
+        'SGD' => [
+            'name' => 'Singapore Dollar',
+            'symbol' => 'S$',
+            'code' => 'SGD'
+        ],
+        'SEK' => [
+            'name' => 'Swedish Krona',
+            'symbol' => 'kr',
+            'code' => 'SEK'
+        ],
+        'CHF' => [
+            'name' => 'Swiss Franc',
+            'symbol' => 'CHF',
+            'code' => 'CHF'
+        ],
+        'THB' => [
+            'name' => 'Thai Baht',
+            'symbol' => '฿',
+            'code' => 'THB'
+        ],
+        'USD' => [
+            'name' => 'United States Dollar',
+            'symbol' => '$',
+            'code' => 'USD'
+        ]
+        ],
+
+        'stripe_currencies' => [
+            'USD', 'AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN', 
+            'BAM', 'BBD', 'BDT', 'BGN', 'BIF', 'BMD', 'BND', 'BOB', 'BRL', 'BSD', 'BWP', 
+            'BYN', 'BZD', 'CAD', 'CDF', 'CHF', 'CNY', 'COP', 'CRC', 'CVE', 'CZK', 'DJF', 
+            'DKK', 'DOP', 'DZD', 'EGP', 'ETB', 'EUR', 'FJD', 'FKP', 'GBP', 'GEL', 'GIP', 
+            'GMD', 'GNF', 'GTQ', 'GYD', 'HKD', 'HNL', 'HRK', 'HTG', 'HUF', 'IDR', 'ILS', 
+            'INR', 'ISK', 'JMD', 'JPY', 'KES', 'KGS', 'KHR', 'KMF', 'KRW', 'KYD', 'KZT', 
+            'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'MAD', 'MDL', 'MGA', 'MKD', 'MMK', 'MNT', 
+            'MOP', 'MRO', 'MUR', 'MVR', 'MWK', 'MXN', 'MYR', 'MZN', 'NAD', 'NGN', 'NIO', 
+            'NOK', 'NPR', 'NZD', 'PAB', 'PEN', 'PGK', 'PHP', 'PKR', 'PLN', 'PYG', 'QAR', 
+            'RON', 'RSD', 'RUB', 'RWF', 'SAR', 'SBD', 'SCR', 'SEK', 'SGD', 'SHP', 'SLL', 
+            'SOS', 'SRD', 'STD', 'SZL', 'THB', 'TJS', 'TOP', 'TRY', 'TTD', 'TWD', 'TZS', 
+            'UAH', 'UGX', 'UYU', 'UZS', 'VND', 'VUV', 'WST', 'XAF', 'XCD', 'XOF', 'XPF', 
+            'YER', 'ZAR', 'ZMW'
+        ],
+        
+        'razorpay_currencies' => [
+            'INR', 'USD', 'EUR', 'GBP', 'SGD', 'AED', 'CAD', 'CNY', 
+            'SEK', 'NZD', 'CHF', 'DKK', 'JPY', 'MYR', 'SAR', 'IDR'
+        ]
+
+];
