aa72f50b99b1fb1de9e8da712439b2114dd66941
vit-posmourny <dr1v3r@email.cz>
2025-10-29 01:48:31 +0100

    evething is ok, but the title still needs to be centered

--- app/Http/Controllers/Admin/CertificateController.php
+++ app/Http/Controllers/Admin/CertificateController.php
@@ -2,24 +2,35 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Course;
+use Barryvdh\DomPDF\Facade\Pdf;
 use App\Models\CertificateBuilder;
 use App\Http\Controllers\Controller;
 use App\Models\CertificateBuilderItem;
-use Barryvdh\DomPDF\Facade\Pdf;
 
-use Dompdf\Dompdf;
 
 require '../vendor/autoload.php';
 
 class CertificateController extends Controller
 {
 
-    function index()
+    function download($id)
     {
+        $course = Course::findOrFail($id);
         $certificate = CertificateBuilder::first();
         $certificateItems = CertificateBuilderItem::all();
 
-        $pdf = Pdf::loadView('frontend.student-dashboard.enrolled-courses.certificate', compact('certificate', 'certificateItems'))->stream();
-        return $pdf;
+        $html = view('frontend.student-dashboard.enrolled-courses.certificate', compact('certificate', 'certificateItems'))->render();
+        $html = str_replace("[student_name]", user()->name, $html);
+        $html = str_replace("[course_name]", $course->title, $html);
+        $html = str_replace("[date]", date('d-m-Y'), $html);
+        $html = str_replace("[platform_name]", 'EduCore', $html);
+        $html = str_replace("[instructor_name]", $course->instructor->name, $html);
+
+        $pdf = pdf::loadHTML($html);
+        return $pdf->download('certificate.pdf');
+
+        // $pdf = Pdf::loadView('frontend.student-dashboard.enrolled-courses.certificate', compact('certificate', 'certificateItems'))->stream();
+        // return $pdf;
     }
 }
