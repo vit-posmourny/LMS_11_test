14198599e5fa1860f369d502efa87c8d4a55f662
vit-posmourny <dr1v3r@email.cz>
2025-12-20 18:50:13 +0100

    refactor(blade): content of views/laouts + app.js

--- resources/views/layouts/admin-app.blade.php
+++ resources/views/layouts/admin-app.blade.php
@@ -1,4 +1,4 @@
-{{-- resources\views\layouts\admin-app.blade.php --}}
+<!-- resources\views\layouts\admin-app.blade.php -->
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
