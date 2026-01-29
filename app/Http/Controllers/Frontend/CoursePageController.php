a5484e13679abda76b4c8d90f410d48528e9b8ef
vit-posmourny <dr1v3r@email.cz>
2026-01-29 14:50:04 +0100

    51 - 1. 1_Course Page - Working With Filtering (Part -1)

--- app/Http/Controllers/Frontend/CoursePageController.php
+++ app/Http/Controllers/Frontend/CoursePageController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Models\Course;
+use App\Models\CourseLevel;
 use Illuminate\Http\Request;
+use App\Models\CourseCategory;
 use Illuminate\Contracts\View\View;
 use App\Http\Controllers\Controller;
+use App\Models\CourseLanguage;
 
 class CoursePageController extends Controller
 {
@@ -15,7 +18,11 @@ class CoursePageController extends Controller
             ->where('status', 'active')
             ->paginate(12);
 
-        return view('frontend.pages.courses-page', compact('courses'));
+        $categories = CourseCategory::where('status', 1)->whereNull('parent_id')->get();
+        $levels = CourseLevel::all();
+        $languages = CourseLanguage::all();
+
+        return view('frontend.pages.courses-page', compact('courses', 'categories', 'levels', 'languages'));
     }
 
 
