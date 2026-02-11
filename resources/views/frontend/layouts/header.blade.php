29407b2c28491e3108494624f740f19b25042fdc
vit-posmourny <dr1v3r@email.cz>
2026-02-11 22:54:36 +0100

    52 - 3. 3_Top Bar - Working With Top bar (Part -3)

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,4 +1,6 @@
-{{-- resources\views\frontend\layouts\header.blade.php --}}
+@php
+    $topBar = App\Models\TopBar::first();
+@endphp
 
     <!--===========================
         HEADER START
@@ -7,38 +9,20 @@
         <div class="row">
             <div class="col-xxl-4 col-lg-7 col-md-8 d-none d-md-block">
                 <ul class="wsus__header_left d-flex flex-wrap">
-                    <li><a href="#"><i class="fab fa-facebook-f"></i> 240k Followers</a></li>
-                    <li><a href="callto:+8806545643"><i class="fas fa-phone-alt"></i> +880 654 - 5643</a></li>
+                    <li><a href="mailto:{{ $topBar->email }}"><x-tabler-icon icon="mail" class="icon-tabler"/>{{ $topBar->email }}</a></li>
+                    <li><a href="callto:{{ $topBar->phone }}"><x-tabler-icon icon="phone" class="icon-tabler" sprite="outline"/> {{ $topBar->phone }}</a></li>
                     <li><a href="#"><i class="fab fa-instagram"></i> 58k Followers</a></li>
                 </ul>
             </div>
             <div class="col-xxl-5 col-lg-7 d-none d-xxl-block">
                 <div class="wsus__header_center">
-                    <p> <span>Limited-Time Special</span> Use Code to Get 30% Off Your First Buying EduCore. <a
-                            href="#">Find out more!</a></p>
+                    <p>
+                        <span> {{ $topBar->offer_name }}</span> {{ $topBar->offer_short_description }} <a href="{{ $topBar->offer_button_url }}">{{ $topBar->offer_button_text }}</a>
+                    </p>
                 </div>
             </div>
             <div class="col-xxl-3 col-lg-5 col-md-4">
-                <ul class="wsus__header_right d-flex flex-wrap">
-                    <li>
-                        <select class="select_js">
-                            <option value="">English </option>
-                            <option value="">Japanese</option>
-                            <option value="">Korean</option>
-                            <option value="">Chinese</option>
-                            <option value="">Urdu</option>
-                        </select>
-                    </li>
-                    <li>
-                        <select class="select_js">
-                            <option value="">$USD</option>
-                            <option value="">₹INR</option>
-                            <option value="">¥CNY</option>
-                            <option value="">€EUR</option>
-                            <option value="">฿THB</option>
-                        </select>
-                    </li>
-                </ul>
+
             </div>
         </div>
     </header>
