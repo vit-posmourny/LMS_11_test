aaea3203dbeb71992f27ab69b1b6fbea722db88c
vit-posmourny <dr1v3r@email.cz>
2026-03-13 23:15:09 +0100

    refactor(admin): +,<,floppy buttons to svg sprite

--- resources/views/admin/course/category/sub-category/index.blade.php
+++ resources/views/admin/course/category/sub-category/index.blade.php
@@ -9,17 +9,11 @@
                     <h3 class="card-title">Course Sub-Category: {{$course_category->name}}</h3>
                     <div class="card-actions">
                         <a href="{{ route('admin.course-categories.index') }}" class="btn btn-outline-primary">
-                            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 6l-6 6l6 6" /></svg>
+                            <x-tabler-icon icon="chevron-left" style="stroke-width: 2" class="icon-tabler" sprite="outline"/>
                             Back
                         </a>
                         <a href="{{ route('admin.sub-categories.create', $course_category->id) }}" class="btn btn-primary">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <path d="M12 5l0 14"></path>
-                                <path d="M5 12l14 0"></path>
-                            </svg>
+                            <x-tabler-icon icon="plus" style="stroke-width: 2;" class="icon-tabler" sprite="outline"/>
                             Add new
                         </a>
                     </div>
@@ -57,29 +51,10 @@
                                         </td>
                                         <td>
                                             <a href="{{ route('admin.sub-categories.edit', ['course_category' => $course_category->id, 'course_sub_category' => $category->id]) }}" class="btn-sm btn-primary">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                                    class="icon icon-tabler icons-tabler-outline icon-tabler-edit">
-                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                    <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
-                                                    <path
-                                                        d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
-                                                    <path d="M16 5l3 3" />
-                                                </svg>
+                                                <x-tabler-icon icon="edit" class="icon-tabler" sprite="outline"/>
                                             </a>
                                             <a href="{{ route('admin.sub-categories.delete', ['course_category' => $course_category->id, 'course_sub_category' => $category->id]) }}" class="text-red delete-item">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                                    stroke-linecap="round" stroke-linejoin="round"
-                                                    class="icon icon-tabler icons-tabler-outline icon-tabler-trash">
-                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                    <path d="M4 7l16 0" />
-                                                    <path d="M10 11l0 6" />
-                                                    <path d="M14 11l0 6" />
-                                                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                                                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                                                </svg>
+                                                <x-tabler-icon icon="trash" class="icon-tabler" sprite="outline"/>
                                             </a>
                                         </td>
                                     </tr>
--- resources/views/admin/footer/column-two/index.blade.php
+++ resources/views/admin/footer/column-two/index.blade.php
@@ -8,14 +8,8 @@
                     <h3 class="card-title">Footer Column Two</h3>
                     <div class="card-actions">
                         <a href="{{ route('admin.footer-column-two.create') }}" class="btn btn-primary">
-                            <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <path d="M12 5l0 14"></path>
-                                <path d="M5 12l14 0"></path>
-                            </svg>
+
+                            <x-tabler-icon icon="plus" style="stroke-width: 2;" class="icon-tabler" sprite="outline"/>
                             Add new
                         </a>
                     </div>
