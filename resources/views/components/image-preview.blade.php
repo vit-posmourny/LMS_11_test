5ed53952797dee5805a1afe9ceaaa2a3c1f0946a
vit-posmourny <dr1v3r@email.cz>
2026-03-24 19:19:15 +0100

    refactor(component): rework image-preview to overflow images

--- resources/views/components/image-preview.blade.php
+++ resources/views/components/image-preview.blade.php
@@ -1,2 +1,12 @@
-{{-- resources\views\components\image.preview.blade.php --}}
-<img {{ $attributes->merge(['class' => 'img-fluid mb-3', 'style' => 'width:150px !important; height:150px !important; object-fit:cover !important;']) }}>
+{{-- resources/views/components/image-preview.blade.php --}}
+<div {{ $attributes->merge(['class' => 'overflow-x-auto overflow-y-hidden max-w-full mb-3']) }} style="height: 150px !important;">
+    <img
+        src="{{ $src ?? '#' }}"
+        class="block object-contain"
+        style="
+            height: auto !important;
+            max-height: 150px !important;
+            width: auto !important;
+            max-width: none !important;
+        ">
+</div>
